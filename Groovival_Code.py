5791b35dea2f70c92dfc55695f12d238657a8428
Amanda <88062177+amandahn@users.noreply.github.com>
2022-07-21 16:26:15 -0700

    Update Groovival_Code.py

--- Groovival_Code.py
+++ Groovival_Code.py
@@ -4,7 +4,7 @@ Groovival
 Author: Amanda Ngo
 Description: Responds to commands given in a discord server to play music
 Created: June 14, 2022
-Last Modified: July 20, 2022
+Last Modified: July 21, 2022
 Inspired by: https://youtu.be/ml-5tXRmmFk and https://youtu.be/jHZlvRr9KxM
 
 """
@@ -16,15 +16,21 @@ import youtube_dl
 class music(commands.Cog):
     def __init__(self, client):
         self.client = client
- 
+    
+    # Description: Bot joins the user's current voice channel
+    # Precondition: User must be in a voice channel
     @commands.command()
     async def join(self,ctx):
        await ctx.author.voice.channel.connect()
-
+    
+    # Description: Bot leaves the user's current voice channel
+    # Precondition: User must be in a voice channel
     @commands.command()
     async def leave(self, ctx):
         await ctx.voice_client.disconnect()
-
+    
+    # Description: Bot streams the given youtube link into the voice channel
+    # Precondition: User must be in a voice channel; must be a valid youtube link
     @commands.command()
     async def play(self, ctx, url):
         FFMPEG_OPTIONS = {"before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5", "options": "-vn"}
@@ -37,23 +43,30 @@ class music(commands.Cog):
             source = await discord.FFmpegOpusAudio.from_probe(url_ydl, **FFMPEG_OPTIONS)
         vc.play(source)
     
+    # Description: Bot sends a description of its commands into a message channel
     @commands.command()
     async def info(self, ctx):
         await ctx.send("**REQUIREMENTS: user must be in a voice channel to use the following commands**\n**!join** ➞ Groovival joins the channel\n\
-                        **!disconnect** ➞ Groovival leaves the channel\n**!play** some url ➞ Streams the given url into the server (MUST BE A YOUTUBE URL)\n
-                            if a given url does not work, try another link of the same song")
-        # Backspace at **!disconnect** until \n for best print formatting
-    
+                        **!disconnect** ➞ Groovival leaves the channel\n**!play** some url ➞ Streams the given url into the server (MUST BE A YOUTUBE URL)\n\
+                        **!pause** ➞ Pauses a given song, if not currently paused\n**!unpause** ➞ Unpauses a given song, if currently paused\n\
+                        **!stop** ➞ Stops a current playing song, if music is currently playing\n
+
+    # Description: Bot pauses the current song being played
+    # Precondition: Audio must be currently plaiyng
     @commands.command()
     async def pause(self, ctx):
         await ctx.voice_client.pause()
         await ctx.send("Music paused")
-
+    
+    # Description: Bot unpauses the current song
+    # Precondition: Audio must be currently paused
     @commands.command()
     async def unpause(self, ctx):
         await ctx.voice_client.resume()
         await ctx.send("Music unpaused")
-        
+    
+    # Description: Bot stop the current song
+    # Precondition: Audio must be currently plaiyng
     @commands.command()
     async def stop(self, ctx):
         await ctx.voice_client.stop()
